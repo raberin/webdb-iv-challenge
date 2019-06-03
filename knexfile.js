7894b0f8d87267140a5a6dadd370d6655ee3f9c3
raberin <rmvaberin@gmail.com>
2019-06-02 17:16:03 -0700

    Initialized the repo, created json file, created index.js, server.js, knex.js

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/lambda.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    //Need this since SQLite does not enforce FK's by default
+    pool: {
+      afterCreate: (connection, done) => {
+        connection.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  }
+};
